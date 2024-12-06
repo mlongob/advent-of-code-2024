ec70139f2bd05c7b446e99aee59e2a0eb517dd45
Mario Longobardi <mlongobardi2@bloomberg.net>
2024-12-06 18:25:00 -0500

    Day 2: part one solve

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,10 +1,29 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(2);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+type Report = Vec<u32>;
+
+fn parse_report(l: &str) -> Report {
+    l.split_ascii_whitespace().filter_map(|x| x.parse().ok()).collect()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+fn validate(report: &Report) -> bool {
+    let increasing = report [0] < report [1];
+    report.iter().tuple_windows().all(|(prev, next) | {
+        if (increasing && next <= prev) || (!increasing && next >= prev) {
+            return false;
+        }
+        let diff = prev.abs_diff(*next);
+        diff >=1 && diff <= 3
+    })
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    Some(input.lines().map(parse_report).filter(|r|validate(r)).count())
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
     None
 }
 
@@ -15,7 +34,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 
     #[test]
