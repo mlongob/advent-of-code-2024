beccf5716a79a544efde5741ed81f7433276d0e9
Mario Longobardi <mlongobardi2@bloomberg.net>
2024-12-07 14:15:10 -0500

    Day 4: solve part 1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,7 +1,96 @@
+use itertools::{iproduct, Itertools};
+use std::str::FromStr;
+
+use advent_of_code::{Direction, Point};
+
 advent_of_code::solution!(4);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+#[derive(Debug)]
+pub struct Matrix {
+    pub cells: Vec<Vec<char>>,
+    pub cols: usize,
+    pub rows: usize,
+}
+
+const CHARS: [char; 4] = ['X', 'M', 'A', 'S'];
+
+impl Matrix {
+    fn get(&self, point: &Point) -> char {
+        self.cells[point.y as usize][point.x as usize]
+    }
+
+    fn safe_get(&self, point: &Point) -> Option<char> {
+        if self.point_inside(point) {
+            Some(self.cells[point.y as usize][point.x as usize])
+        } else {
+            None
+        }
+    }
+
+    fn point_inside(&self, point: &Point) -> bool {
+        point.x >= 0 && point.x < self.cols as isize && point.y >= 0 && point.y < self.rows as isize
+    }
+
+    fn starts(&self) -> Vec<Point> {
+        iproduct!(0..self.cols, 0..self.rows)
+            .filter_map(|(x, y)| {
+                let point = Point {
+                    x: x as isize,
+                    y: y as isize,
+                };
+                const START: char = CHARS[0];
+                match self.get(&point) {
+                    START => Some(point),
+                    _ => None,
+                }
+            })
+            .collect()
+    }
+}
+
+impl FromStr for Matrix {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let cells: Vec<Vec<char>> = s
+            .lines()
+            .filter_map(|line| {
+                if line.is_empty() {
+                    None
+                } else {
+                    Some(line.chars().collect())
+                }
+            })
+            .collect();
+
+        let rows = cells.len();
+        let cols = cells[0].len();
+
+        Ok(Matrix { cells, rows, cols })
+    }
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let matrix: Matrix = input.parse().ok()?;
+    Some(
+        matrix
+            .starts()
+            .into_iter()
+            .cartesian_product(Direction::ALL_DIRECTIONS)
+            .filter(|(start, direction)| {
+                CHARS
+                    .iter()
+                    .try_fold(*start, |point, c| {
+                        if matrix.safe_get(&point)? != *c {
+                            None
+                        } else {
+                            Some(point.neighbor(*direction))
+                        }
+                    })
+                    .is_some()
+            })
+            .count(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,70 @@
 pub mod template;
 
-// Use this file to add helper functions and additional modules.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Direction {
+    N,
+    E,
+    S,
+    W,
+    NE,
+    SE,
+    SW,
+    NW,
+}
+impl Direction {
+    pub const ALL_DIRECTIONS: [Direction; 8] = [
+        Direction::N,
+        Direction::E,
+        Direction::S,
+        Direction::W,
+        Direction::NW,
+        Direction::NE,
+        Direction::SE,
+        Direction::SW,
+    ];
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Point {
+    pub x: isize,
+    pub y: isize,
+}
+
+impl Point {
+    pub fn neighbor(&self, direction: Direction) -> Point {
+        match direction {
+            Direction::N => Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Direction::E => Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Direction::S => Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Direction::W => Point {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Direction::NE => Point {
+                x: self.x + 1,
+                y: self.y - 1,
+            },
+            Direction::NW => Point {
+                x: self.x - 1,
+                y: self.y - 1,
+            },
+            Direction::SE => Point {
+                x: self.x + 1,
+                y: self.y + 1,
+            },
+            Direction::SW => Point {
+                x: self.x - 1,
+                y: self.y + 1,
+            },
+        }
+    }
+}
