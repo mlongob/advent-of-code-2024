9cf4c867fb297b1dd663e637db9f7f0f227cedd3
Mario Longobardi <mlongobardi2@bloomberg.net>
2024-12-08 10:44:07 -0500

    Day 7: solve

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,5 +1,53 @@
 advent_of_code::solution!(7);
 
+#[derive(Clone, Copy, Debug)]
+enum Operator {
+    Add,
+    Mul,
+    Concat,
+}
+
+fn eval(
+    target_value: u64,
+    pool: &[Operator],
+    values: &[u64],
+    index: usize,
+    current_value: u64,
+    op: Option<Operator>,
+) -> bool {
+    if index == values.len() {
+        return current_value == target_value;
+    }
+
+    let next_value = match op {
+        Some(op) => {
+            let x = current_value;
+            let y = values[index];
+            match op {
+                Operator::Add => x + y,
+                Operator::Mul => x * y,
+                Operator::Concat => x * 10u64.pow(y.ilog10() + 1) + y,
+            }
+        }
+        None => values[index],
+    };
+
+    if next_value > target_value {
+        return false;
+    }
+
+    pool.iter()
+        .any(|op| eval(target_value, pool, values, index + 1, next_value, Some(*op)))
+}
+
+fn solve(input: &str, operators: &[Operator]) -> u64 {
+    parse(input)
+        .into_iter()
+        .filter(|(result, values)| eval(*result, operators, values, 0, 0, None))
+        .map(|(result, _)| result)
+        .sum()
+}
+
 fn parse(input: &str) -> Vec<(u64, Vec<u64>)> {
     input
         .lines()
@@ -16,12 +64,14 @@ fn parse(input: &str) -> Vec<(u64, Vec<u64>)> {
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    dbg!(parse(input));
-    None
+    Some(solve(input, &[Operator::Add, Operator::Mul]))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    Some(solve(
+        input,
+        &[Operator::Add, Operator::Mul, Operator::Concat],
+    ))
 }
 
 #[cfg(test)]
@@ -37,6 +87,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
